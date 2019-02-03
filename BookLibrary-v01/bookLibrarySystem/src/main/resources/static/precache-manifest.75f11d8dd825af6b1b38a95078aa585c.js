6b46e67350841e6e7eef3aba8ed5d54a951f6c85
Subhash Kumar <subhash.kumar307@gmail.com>
2019-02-04 00:36:29 +0530

    Added book library system

---
+++ BookLibrary-v01/bookLibrarySystem/src/main/resources/static/precache-manifest.75f11d8dd825af6b1b38a95078aa585c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "70e09e74ed97c082edda",
+    "url": "/static/js/main.70e09e74.chunk.js"
+  },
+  {
+    "revision": "2906c3fc7ecde91df19b",
+    "url": "/static/js/1.2906c3fc.chunk.js"
+  },
+  {
+    "revision": "70e09e74ed97c082edda",
+    "url": "/static/css/main.a1b7f271.chunk.css"
+  },
+  {
+    "revision": "ddd2ffda082afb42ee9c199ca54bbc95",
+    "url": "/index.html"
+  }
+];
